6badbe58333a58656a22597fe84bdecaa208dcd2
Zhikang Fan <zhikangfan@outlook.com>
2025-09-11 09:36:23 +0800

    build: 更新发布脚本并添加发布说明
    - 在 publish.sh 中添加了变量定义，用于配置 tar 文件名和 dist 目录- 注释掉了创建 tar 文件的命令，改为直接发送 dist 目录内容到服务器
    - 更新了 README.md，添加了发布步骤说明

---
+++ README.md
@@ -0,0 +1,10 @@
+## 发布
+1. 本地运行
+```shell
+./scripts/publish.sh
+```
+2. 服务器运行
+```shell
+cd /home/wwwroot
+./publish.sh
+```
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,6 +2,10 @@
 #切换到项目根目录
 cd "$(dirname "$0")"/..
 
+# 变量
+TAR_FILE="lucky.tar.gz"
+DIST_DIR="dist"
+
 # 加载nvm环境
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # 这将加载nvm
@@ -9,4 +13,7 @@ export NVM_DIR="$HOME/.nvm"
 
 nvm use
 npm run build
+# tar -czf ${TAR_FILE} -C ${DIST_DIR} .
+# 发送dist目录到服务器
 scp -r dist/* root@47.101.31.109:/home/wwwroot/lucky/
+# scp -r ${TAR_FILE} root@47.101.31.109:/home/wwwroot/
